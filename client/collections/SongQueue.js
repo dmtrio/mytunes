b3c8e111dd261d1b86e587dd98852bc4ef03d3ad
Hack Reactor Student <student@hackreactor.com>
2017-07-10 20:01:19 -0500

    pass mocha tests for SongModel, SongQueue, SongQueueEntry, SongQueueView, LibraryEntry, LibraryEntryView

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,24 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    var context = this;
+    context.on('add', function() {
+      if (context.length === 1 ) {
+        context.playFirst();
+      }
+    }, this);
+    context.on('ended', function() {
+      context.shift();
+      if (context.length >= 1) {
+        context.playFirst();
+      }
+    }, this);
+    context.on('dequeue', function() {
+      context.remove(context.where(this));
+    }, this);
+  },
 
+  playFirst: function() {
+    this.at(0).play();
+  },
 });
