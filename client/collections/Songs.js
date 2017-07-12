cd1d6c08ad9522955000cd8edcac796081735ee9
Hack Reactor Student <student@hackreactor.com>
2017-07-11 20:17:15 -0500

    added Parse API songs

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -5,26 +5,34 @@ var Songs = Backbone.Collection.extend({
   url: 'http://parse.atx.hackreactor.com/mytunes/classes/songs',
   
   initialize: function() {
-    //this.fetchPARSE();
+    this.fetch();
   },
   
-  fetchPARSE: function() {
-    this.fetch({
-      success: this.fetchSuccess,
-      error: this.fetchError,
-    });
-  },
-
-  fetchSuccess: function(collection, response) {
-    // for (var result of response.results) {
-    //   this.get('library').add(result);
-      
-    // }
-    console.log('collection fetchSuccess', response);
-  },
   
-  fetchError: function(collection, response) {
-    throw new Error('fetchError');
+  // fetchPARSE: function() {
+  //   console.log('col before', this);
+  //   this.fetch({
+  //     reset: true,
+  //     success: this.fetchSuccess,
+  //     error: this.fetchError,
+  //   });
+  // },
+  
+  parse: function(response) {
+    return response.results;
   }
+
+  // fetchSuccess: function(collection, response) {
+  //   console.log('col after', collection);
+  //   // for (var result of response.results) {
+  //   //   this.get('library').add(result);
+      
+  //   // }
+  //   console.log('collection fetchSuccess', response);
+  // },
+  
+  // fetchError: function(collection, response) {
+  //   throw new Error('fetchError');
+  // }
 
 });
